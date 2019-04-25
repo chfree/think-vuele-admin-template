3ebf78b4123594affcc128242b1f42c9090e6201
zfree <79763939@qq.com>
2019-04-25 13:03:44 +0800

    登陆的布局骨架

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,15 @@ export default {
 </script>
 
 <style>
+body{
+  padding: 0px !important;
+  margin: 0px !important;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/views/page/login/index.vue
+++ src/views/page/login/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    登陆
+  <div class="login-container">
+    <div class="login-card">
+
+    </div>
   </div>
 </template>
 
@@ -10,5 +12,22 @@
 </script>
 
 <style lang="scss" scoped>
-
+.login-container{
+  background-color: rgb(174, 203, 230);
+  height: 100vh;
+}
+$login-card-width:440px;
+$login-card-height:260px;
+.login-card{
+  position: absolute;
+  width: $login-card-width;
+  height: $login-card-height;
+  background-color: #fff;
+  border:1px solid #333;
+  border-radius: 5px;
+  left: 50%;
+  top: 50%;
+  margin-left: -$login-card-width/2;
+  margin-top: -$login-card-height/2;
+}
 </style>
