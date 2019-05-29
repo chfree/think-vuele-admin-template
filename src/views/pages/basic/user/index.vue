f5b40a736d4ac36993dd8a17590be8eef609b2f5
zfree <79763939@qq.com>
2019-05-29 12:59:50 +0800

    用户管理界面布局

--- src/views/pages/basic/user/index.vue
+++ src/views/pages/basic/user/index.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
-    <tc-block style="padding:5px;">
+    <tc-block title="搜索" style="margin-bottom:5px;">
+      <tc-form size="small" label-width="100px">
+        <el-row>
+          <el-col :span="8">
+            <tc-form-item label="用户姓名:">
+              <tc-input v-model="searchForm.usrNm"></tc-input>
+            </tc-form-item>
+          </el-col>
+          <el-col :span="8">
+            <tc-form-item label="账号:">
+              <tc-input v-model="searchForm.empeIdLandNm"></tc-input>
+            </tc-form-item>
+          </el-col>
+          <el-col :span="8">
+            <tc-form-item label="电话:">
+              <tc-input v-model="searchForm.empeIdLandNm"></tc-input>
+            </tc-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24" style="text-align:center">
+            <tc-button type="primary" size="small" icon="el-icon-search">查询</tc-button>
+            <tc-button type="primary" size="small" icon="el-icon-search">重置</tc-button>
+          </el-col>
+        </el-row>
+      </tc-form>
+    </tc-block>
+    <tc-block style="margin-bottom:5px;">
+      <div style="margin-bottom:10px;">
+        <el-button-group>
+          <el-button type="primary" size="small" icon="el-icon-edit-outline">新增</el-button>
+          <el-button type="primary" size="small" icon="el-icon-edit-outline">编辑</el-button>
+          <el-button type="primary" size="small" icon="el-icon-edit-outline">查看</el-button>
+          <el-button type="primary" size="small" icon="el-icon-edit-outline">分配角色</el-button>
+        </el-button-group>
+      </div>
       <tc-table :data="data.list" :columns="columns" />
     </tc-block>
   </div>
@@ -18,6 +53,8 @@ export default {
         {name: 'rmrk', text: '备注'}],
       data: {
         list: []
+      },
+      searchForm: {
       }
     }
   },
