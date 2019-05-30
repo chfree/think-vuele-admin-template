9458c5da866a7883b1854e1e91eee70a93b229b0
zfree <79763939@qq.com>
2019-05-30 23:56:33 +0800

    添加了详情的布局

--- src/views/pages/basic/user/detailed.vue
+++ src/views/pages/basic/user/detailed.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-
+    <tc-form :model="userModel" size="small" label-width="80px">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="姓名">
+            <el-input v-model="userModel.name"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="账号">
+            <el-input v-model="userModel.account"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="性别">
+            <el-input v-model="userModel.sex"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <tc-fixed-bottom style="text-align:center;line-height:30px;">
+        <tc-button type="think" size="small">保存</tc-button>
+        <tc-button type="think" size="small" @click="resetForm">重置</tc-button>
+      </tc-fixed-bottom>
+    </tc-form>
   </div>
 </template>
 
@@ -11,6 +33,11 @@ export default {
   data() {
     return {
 
+    }
+  },
+  methods: {
+    resetForm() {
+      this.resetModel()
     }
   }
 }
