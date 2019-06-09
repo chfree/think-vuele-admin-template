2379a8f24992e0b063fe3682bd6a9f8a64192278
zfree <79763939@qq.com>
2019-06-09 09:59:09 +0800

    跳转了cypress的文件位置

--- test/cypress/integration/login/login.spec.js
+++ test/cypress/integration/login/login.spec.js
@@ -1,5 +1,5 @@
-describe('My First Test', function() {
-  it('Visits the bing.com', function() {
+describe('登陆测试', function() {
+  it('测试登陆输入正确是否成功跳转', function() {
     cy.visit('http://localhost:8091/#/')
     cy.get('input[name="username"]')
       .clear()
